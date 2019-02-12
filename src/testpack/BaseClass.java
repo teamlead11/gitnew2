a25e09a63a3c7bb7dd4ec4b159fcbfce5c8be855
YuvarajSekar3 <yuvi.sekar3@gmail.com>
2019-02-12 21:35:55 +0530

    firstcommit

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -4,10 +4,10 @@ public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		baseclassMethods();
+		Methods();
 	}
 
-	public static void baseclassMethods() {
+	public static void Methods() {
 		System.out.println("3rd modified logic");
 	}
 
---
+++ src/testpack/TestCase2.java
@@ -0,0 +1,10 @@
+package testpack;
+
+public class TestCase2 {
+	public static void main(String[] args) {
+		
+	}
+ public static void gitHubMethod() {
+	 
+ }
+}
