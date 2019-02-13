3e6d101e7936f7877a4bc5f6e4b480d3a14c08a8
Hameedshahulh <hameed15shahulh@gmail.com>
2019-02-13 12:44:09 +0530

    sorting an array by shahul

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,5 +1,11 @@
 package testpack;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
@@ -8,7 +14,7 @@ public class BaseClass {
 		BaseClass base = new BaseClass();
 		base.add(4, 5);
 		base.add(1, 2, 3);
-
+		base.sortAnArrayByShahul();
 	}
 
 	public static void baseclassMethods() {
@@ -31,4 +37,22 @@ public class BaseClass {
 	public void add(String a, String b, String c) {
 		System.out.println(a + b + c);
 	}
+	
+	public void sortAnArrayByShahul() {
+		List<Integer> a= new ArrayList<Integer>();
+		a.add(5);
+		a.add(6);
+		a.add(1);
+		a.add(4);
+		System.out.println("Before sort");
+		for (Object li : a) {
+			System.out.println(li);
+		}
+		Collections.sort(a);
+		System.out.println("After sort");
+		for (Integer ali : a) {
+			System.out.println(ali);
+		}
+		
+		}
 }
