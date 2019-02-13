78bcb7aa266bed550062e4d731bde49b201d6785
teamlead11 <automationlead11@gmail.com>
2019-02-13 10:44:06 +0530

    user1 changes

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -5,10 +5,15 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		baseclassMethods();
+		BaseClass b = new BaseClass();
+		b.add(1, 2, 3);
 	}
 
 	public static void baseclassMethods() {
 		System.out.println("3rd modified logic");
 	}
 
+	public void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
